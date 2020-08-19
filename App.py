42f228683babeb6eaace84d66381c2da1ce0f445
Cesar Hernandez <devcesarhdez@gmail.com>
2020-08-19 14:45:40 -0500

    insercion en base de datos

--- App.py
+++ App.py
@@ -1,20 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = '192.168.0.100'
-app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flaskcontacts'
+app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 mysql = MySQL(app)
 
 @app.route('/')
 def Index():
-    return render_template('layout.html');
+    return render_template('form.html')
 
-@app.route('/add_contact')
+@app.route('/add_contact', methods=['POST'])
 def add_contact():
-    return "Add Contact"
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO contacts (fullname, phone, email) VALUES (%s, %s, %s)', (fullname, phone, email))
+        mysql.connection.commit()
+        flash('contact added successfully')
+        return redirect(url_for('Index'))
 
 @app.route('/edit')
 def edit_contact():
@@ -23,6 +36,10 @@ def edit_contact():
 @app.route('/delete')
 def delete():
     return "delete contact"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'This page does not exist', 404
 
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
