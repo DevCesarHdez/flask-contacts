82bc9b4024c8354c12c0626ad42f45aa6b216a36
Cesar Hernandez <devcesarhdez@gmail.com>
2020-08-19 18:36:46 -0500

    CRUD LISTO

--- App.py
+++ App.py
@@ -7,15 +7,23 @@ load_dotenv()
 
 
 app = Flask(__name__)
+
+#MySQL connection
 app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
 app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 mysql = MySQL(app)
 
+#settings
+app.secret_key = 'mysecretkey'
+
 @app.route('/')
 def Index():
-    return render_template('form.html')
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contacts')
+    data = cur.fetchall()
+    return render_template('form.html', contacts = data)
 
 @app.route('/add_contact', methods=['POST'])
 def add_contact():
@@ -29,13 +37,39 @@ def add_contact():
         flash('contact added successfully')
         return redirect(url_for('Index'))
 
-@app.route('/edit')
-def edit_contact():
-    return "edit contact"
+@app.route('/edit/<id>')
+def get_contact(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT idcontacts, fullname, phone, email FROM contacts WHERE idcontacts = {0}'.format(id))
+    data = cur.fetchall()
+    print(data[0])
+    return render_template('edit-contact.html', contact = data[0])
 
-@app.route('/delete')
-def delete():
-    return "delete contact"
+@app.route('/update/<id>', methods=['POST'])
+def update_contact(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute("""
+        UPDATE contacts
+        SET fullname = %s,
+            phone = %s,
+            email = %s
+        WHERE idcontacts = %s
+        """,(fullname, phone, email, id))
+        mysql.connection.commit()
+        flash('contact updated successfully')
+        return redirect(url_for('Index'))
+
+@app.route('/delete/<string:id>')
+def delete(id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM contacts WHERE idcontacts = {0}'.format(id))
+    mysql.connection.commit()
+    flash('Contact removed sucessfully')
+    return redirect(url_for('Index'))
 
 @app.errorhandler(404)
 def page_not_found(error):
