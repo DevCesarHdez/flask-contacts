169946a3267cac61f651d12a9dcce547c44ac022
Cesar Hernandez <devcesarhdez@gmail.com>
2020-08-19 19:46:51 -0500

    runtime y password de la base dedatos (.env)

--- App.py
+++ App.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 #MySQL connection
 app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
 app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
 app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 mysql = MySQL(app)
 
